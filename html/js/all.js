b13536c82bdaede82feba6d8be4b8d0a353cbce4
tschlich <t.schlich@herzogkommunikation.de>
2014-11-20 23:35:17 +0100

    Create all.js

---
+++ html/js/all.js
@@ -0,0 +1,10 @@
+<button onclick="myFunction()">Try Java script</button>
+function myFunction() {
+    var x;
+    if (confirm("Press a button!") == true) {
+        x = "You pressed OK!";
+    } else {
+        x = "You pressed Cancel!";
+    }
+    document.getElementById("demo").innerHTML = x;
+}
